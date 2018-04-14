311642193aace2cef070fbbbfd17e4e8d92f6347
Antonio Lázaro <antoniolazaro@me.com>
2018-04-14 19:03:06 +0200

    Add PostThought servlet

---
+++ src/es/upm/dit/geoloc/servlets/PostThoughtServlet.java
@@ -0,0 +1,10 @@
+package es.upm.dit.geoloc.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class PostThoughtServlet extends HttpServlet {
+
+}
