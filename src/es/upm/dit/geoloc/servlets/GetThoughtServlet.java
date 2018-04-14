edd4c299fccae0b3912084ee334d9ab40954c2d3
Alejandro Perona <aperonam@gmail.com>
2018-04-14 20:22:04 +0200

    Created GetThoughtServlet file

---
+++ src/es/upm/dit/geoloc/servlets/GetThoughtServlet.java
@@ -0,0 +1,17 @@
+package es.upm.dit.geoloc.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import es.upm.dit.geoloc.dao.*;
+import es.upm.dit.geoloc.dao.model.Thought;
+
+
+public class GetThoughtServlet extends HttpServlet {
+
+    public static void main (String [ ] args) {
+
+
+    }
+}
