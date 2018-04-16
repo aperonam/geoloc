764e2c3ae1dd9d10e77f104c4dcc1bd4dd9573b4
Alejandro Perona <aperonam@gmail.com>
2018-04-16 11:53:35 +0200

    first try at GetThoughtsServlet

--- src/es/upm/dit/geoloc/servlets/GetThoughtsServlet.java
+++ src/es/upm/dit/geoloc/servlets/GetThoughtsServlet.java
@@ -1,17 +1,39 @@
 package es.upm.dit.geoloc.servlets;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.util.List;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import es.upm.dit.geoloc.dao.*;
 import es.upm.dit.geoloc.dao.model.Thought;
 
-
-public class GetThoughtServlet extends HttpServlet {
-
-    public static void main (String [ ] args) {
+@WebServlet("/getThoughts")
+public class GetThoughtsServlet extends HttpServlet {
 
 
-    }
+	    public static void main (String [ ] args) {
+
+	    }
+
+	    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	        
+	    	List<Thought> thoughts = new ArrayList<>();
+	    	
+	    	thoughts = ThoughtDAOImplementation.getInstance().readThoughts();
+	    	request.getSession().setAttribute("thoughts", thoughts);
+	    	response.sendRedirect(request.getContextPath() + "/viewThoughts");
+	    	
+	    	
+	    	
+	    	
+	    	
+	    	
+	    }
 }
+
